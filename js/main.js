c4073f22eb79c95a5feda3a15321a45a6fd3f130
chenchuk77 <chenchuk@gmail.com>
2017-10-20 15:02:49 +0300

    set js to request rest at port 8090

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 $(document).ready(function() {
     $('#asmText').keyup(function(){
         $.ajax({
-            url : 'http://localhost:8080/assemble',
+            url : 'http://localhost:8090/assemble',
             type: "POST",
             dataType: "json",
             data : {
