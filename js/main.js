e4220aae1e988691e1d4c2eccca1cc16086f159a
chenchuk77 <chenchuk@gmail.com>
2017-10-20 16:22:29 +0300

    1. adding js error handling 2. add idea to .gitignore

--- js/main.js
+++ js/main.js
@@ -22,10 +22,24 @@ $(document).ready(function() {
                 //$('#hackText').val(data.source);
                 $('#hackText').val(data.output);
             },
-            error:   function(jqXHR, textStatus, errorThrown) {
-                console.log("Error, status = " + textStatus + ", " +
-                    "error thrown: " + errorThrown
-                );
+            error: function (jqXHR, exception) {
+                var msg = '';
+                if (jqXHR.status === 0) {
+                    msg = 'Not connect.\n Verify Network.';
+                } else if (jqXHR.status == 404) {
+                    msg = 'Requested page not found. [404]';
+                } else if (jqXHR.status == 500) {
+                    msg = 'Internal Server Error [500].';
+                } else if (exception === 'parsererror') {
+                    msg = 'Requested JSON parse failed.';
+                } else if (exception === 'timeout') {
+                    msg = 'Time out error.';
+                } else if (exception === 'abort') {
+                    msg = 'Ajax request aborted.';
+                } else {
+                    msg = 'Uncaught Error.\n' + jqXHR.responseText;
+                }
+                $('#post').html(msg);
             }
 
         })
